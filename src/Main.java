d5b7b3cd3f25642cbfc68dc2ff18d0d8c06d2046
EXPLOREY <alihaj652379@gmail.com>
2025-03-03 22:46:22 +0300

    All Fixed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ class Account {
     int nationalCode;
     String city;
     int bankBranchNum;
-    double accBalance;
+    float accBalance;
     AccountType accountType;
     int score;
 
@@ -13,17 +13,17 @@ class Account {
         NRI;
     }
 
-    public Account(String name, int nationalCode, String city, int bankBranchNum, double accBalance, String accountTypeString) {
+    public Account(String name, int nationalCode, String city, int bankBranchNum, float accBalance, String accountTypeString) {
         this.name = name;
         this.nationalCode = nationalCode;
         this.city = city;
         this.bankBranchNum = bankBranchNum;
         this.accBalance = accBalance;
-        this.accountType = AccountType.valueOf(accountTypeString.toUpperCase());
+        this.accountType = AccountType.valueOf(accountTypeString);
         this.score = 0;
     }
 
-    public Account(String name, int nationalCode, int bankBranchNum, double accBalance, String accountTypeString) {
+    public Account(String name, int nationalCode, int bankBranchNum, float accBalance, String accountTypeString) {
         this(name, nationalCode, "Esfahan", bankBranchNum, accBalance, accountTypeString);
     }
 
@@ -36,106 +36,112 @@ class Account {
         this(name, nationalCode, "Esfahan", bankBranchNum, 2000, accountTypeString);
     }
 
-    public void checkAccountBalance() {
+    public String checkAccountBalance() {
         if (accountType == AccountType.SAVING_ACC && accBalance < 1) {
-            System.out.println("you are Very Poor");
+            return "you are Very Poor";
         } else if (accountType == AccountType.SAVING_ACC) {
             accBalance -= 1;
             score += 10;
-            System.out.println("Your bank account balance : " + accBalance);
+            return "Your bank account balance : " + accBalance;
         }
         if ((accountType == AccountType.SALARY_ACC || accountType == AccountType.NRI) && accBalance < 1) {
-            System.out.println("you are Very Poor");
+            return "you are Very Poor";
         } else if (accountType == AccountType.SALARY_ACC || accountType == AccountType.NRI) {
             accBalance -= 2;
             score += 10;
-            System.out.println("Your bank account balance : " + accBalance);
+            return "Your bank account balance : " + accBalance;
         }
+        return "";
 
     }
 
-    public void withdraw(int pickUp) {
+    public String withdraw(int pickUp) {
         if (pickUp > 10000) {
-            System.out.println("The Withdrawal Limit is 10000$");
-            return;
+            return "The Withdrawal Limit is 10000$";
         }
         if (accBalance < pickUp) {
-            System.out.println("Your Account Balance is Not Enough");
-            return;
+            return "Your Account Balance is Not Enough";
         } else {
             accBalance -= pickUp;
             score += 20;
-            System.out.println("Withdrawal Completed Successfully");
+            return "Withdrawal Completed Successfully";
         }
     }
 
-    public void deposit(int putIn) {
+    public String deposit(int putIn) {
         if (putIn > 1000) {
-            System.out.println("The Deposit Limit is 1000$");
             accBalance -= (accBalance * 0.01);
-            System.out.println("Your balance after fine :" + accBalance);
-            return;
+            return "The Deposit Limit is 1000$ \n" + "Your balance after fine :" + accBalance;
         } else {
             accBalance += putIn;
             score += 30;
-            System.out.println("Deposit was Made Successfully");
+            return "Deposit was Made Successfully";
         }
 
     }
 
-    public void profitCalculation() {
+    public double profitCalculation() {
+        double profit = 0;
         if (accountType == AccountType.SAVING_ACC) {
-            accBalance += (accBalance * 0.1);
+            profit = (accBalance * 0.1);
         } else if (accountType == AccountType.SALARY_ACC || accountType == AccountType.NRI) {
-            accBalance += (accBalance * 0.02);
+            profit = (accBalance * 0.02);
         }
+        return profit;
     }
 
-    public void loanRequest(String name, int NationalCode, int loan) {
-
+    public String loanRequest(String name, int nationalCode, int loan) {
         if (!this.name.equals(name) || this.nationalCode != nationalCode) {
-            System.out.println("Account Not Found");
-            return;
-        }
-        if (accountType == AccountType.SAVING_ACC || accountType == AccountType.SALARY_ACC) {
-            if (loan > 500) {
-                return;
+            return "Account Not Found";
+        } else {
+            if (accountType == AccountType.SAVING_ACC || accountType == AccountType.SALARY_ACC) {
+                if (loan > 500) {
+                    return "Loan Application Applied UnSuccessfully";
+                }
+                if (accBalance / 2 < loan) {
+                    return "Your Credit is not Enough for This Loan";
+                } else if (score < 100) {
+                    return "You don't Have Enough Score";
+                } else {
+                    accBalance += loan;
+                    return "Loan Application Applied Successfully";
+
+                }
             }
-        } else if (accBalance < loan / 2) {
-            System.out.println("Your Credit is not Enough for This Loan");
-            return;
-        } else if (score < 100) {
-            System.out.println("You don't Have Enough Score");
-            return;
-        } else {
-            System.out.println("Loan Application Applied Successfully");
-            accBalance += loan;
+            if (accountType == AccountType.NRI) {
+                if (accBalance / 2 < loan) {
+                    return "Your Credit is not Enough for This Loan";
+                } else if (score < 100) {
+                    return "You don't Have Enough Score";
+
+                } else {
+                    accBalance += loan;
+                    return "Loan Application Applied Successfully";
+                }
+            }
+
+
+            return "";
         }
-
-
     }
 
-    public void loanRequest(String name, int nationalCode, LoansType loanType) {
+
+    public String loanRequest(String name, int nationalCode, String loanType) {
+        LoansType loansType = LoansType.valueOf(loanType);
         if (!this.name.equals(name) || this.nationalCode != nationalCode) {
-            System.out.println("Account Not Found");
-            return;
-        }
-
-        if (loanType == LoansType.BUSINESS) {
-            accBalance += loanType.getAmount();
-        } else if (loanType == LoansType.STUDENT) {
-            accBalance += loanType.getAmount();
-        } else if (loanType == LoansType.MORTAGE) {
-            accBalance += loanType.getAmount();
-        } else if (loanType == LoansType.MARRIGE) {
-            accBalance += loanType.getAmount();
+            return "Account Not Found";
         } else {
-            System.out.println("Invalid Loan Type");
-            return;
+            this.accBalance += loansType.getAmount();
+            return "Loan Application Applied Successfully";
         }
-
-        System.out.println("Loan Application Applied Successfully");
     }
+
+    public double credit = 0;
+
+    public double credit() {
+        return this.accBalance;
+    }
+
 
 }
 
@@ -162,22 +168,19 @@ public class Main {
 
     public static void main(String[] args) {
         Account account1 = new Account("Ftm", 123, "Tehran", 11, 50000, "SAVING_ACC");
-
-        account1.checkAccountBalance();
-        account1.withdraw(5000);
-        account1.deposit(1000);
-        account1.deposit(500);
-        account1.deposit(7000);
-        account1.withdraw(1000);
-        account1.deposit(11000);
-        account1.checkAccountBalance();
-
-        account1.profitCalculation();
-
-        account1.loanRequest("Ftm", 124, 400);
-        account1.loanRequest("Ftm", 123, 400);
-
-        account1.loanRequest("Ftm", 123, LoansType.BUSINESS);
+        System.out.println(account1.checkAccountBalance());
+        System.out.println(account1.withdraw(5000));
+        System.out.println(account1.deposit(1000));
+        System.out.println(account1.deposit(500));
+        System.out.println(account1.deposit(7000));
+        System.out.println(account1.withdraw(1000));
+        System.out.println(account1.deposit(11000));
+        System.out.println(account1.checkAccountBalance());
+        System.out.println(account1.profitCalculation());
+        System.out.println(account1.loanRequest("Ftm", 124, 400));
+        System.out.println(account1.loanRequest("Ftm", 123, 400));
+        System.out.println(account1.loanRequest("Ftm", 123, "BUSINESS"));
+        System.out.println(account1.credit());
     }
 }
 
